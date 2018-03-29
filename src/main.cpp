bd6b210dd1b9a4693d86192b8ff880f926693ebc
@gsergia <gsergia@gmail.com>
2018-03-30 00:32:39 +0400

    Create new windows and show

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
-#include <iostream>
-using namespace std;
+#include <FL/Fl.H>
+//#include <FL/Fl_File_Icon.H>
+#include <FL/Fl_Shared_Image.H>
+#include <main_panel.h>
+#include <render_panel.h>
 
-int main()
+using namespace cvtool;
+
+int main(int  argc,	char *argv[])
 {
-    return 1;
+    /*Fl::scheme("gtk+");
+    choice->add("none");
+  choice->add("gtk+");
+  choice->add("gleam");
+  choice->add("plastic");*/
+
+    Fl::lock();
+
+    Fl::scheme("gtk+");
+    Fl::get_system_colors();
+    Fl::visual(FL_RGB);
+
+    //Fl_File_Icon::load_system_icons();
+    
+    main_panel = MainWnd::make_main_panel();
+
+    render_panel = RenderWnd::make_video_panel(FRAME_WIDTH, FRAME_HEIGHT, "GLFrame");
+    if ((Fl::w() < render_panel->w()) || (Fl::h() < render_panel->h()))
+        render_panel->size(Fl::w(), Fl::h());
+
+    main_panel->show(argc,argv);
+    render_panel->show();
+
+    // Let FLTK run
+    int res = Fl::run();
+
+    // Clean up
+    Fl::unlock();
+    //uavv_image_destroy(buf);
+
+    delete render_panel;
+    delete main_panel;
+
+    return res;
 }
 
